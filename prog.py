a5f774047e2786e6cc414b4892c9a9e77e4f7f39
Borsyakov Daniil <dborsakov@gmail.com>
2019-03-18 08:21:28 +0300

    add H(XY)

--- prog.py
+++ prog.py
@@ -19,10 +19,19 @@ def func_2(matrix):
     res = sum(tmp_2)
     return res*(-1)
 
+def func_3(matrix):
+    """H(XY)"""
+    res = 0
+    for i in matrix:
+        for j in i:
+           res += j*m.log(j)
+    return res
+
 def main():
     s=read.readtxt('sa')
     print(func_1(s))
     print(func_2(s))
+    print(func_3(s))
 
 if __name__ == "__main__":
     main()
